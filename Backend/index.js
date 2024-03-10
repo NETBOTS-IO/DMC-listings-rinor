16faba348afeb0e56ca60dc6bbc74a195f9f467d
Jaffer <mjaffer720@gmail.com>
2024-03-10 14:00:14 +0500

    modify cors

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 
 //middlewares
 
-app.use(cors({origin:["http://127.0.0.1:5173", "http://localhost:5173"], credentials:true }));
+app.use(cors({ origin: ["http://127.0.0.1:5173", "http://localhost:5173", "https://dmc-listings-rinor.vercel.app"], credentials: true }));
 
 app.use(cookieParser());
 app.use(express.json({ limit: '50mb' }));
