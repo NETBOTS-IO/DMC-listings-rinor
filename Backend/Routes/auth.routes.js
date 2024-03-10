b3e60279ef5e847c79b0c4e6bb8f7ebc57517ac1
Jaffer <mjaffer720@gmail.com>
2024-03-10 13:43:46 +0500

    authRoute.js

--- Backend/Routes/auth.routes.js
+++ Backend/Routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login } from "../controllers/auth.js";
+import { register, login } from "../Controllers/auth.js";
 
 
 const router = express.Router();
